ac329730ac227a2e385ba78ccf5ed54e948b519d
ramonzxc1 <ramonalternativeacc@gmail.com>
2023-05-14 21:11:04 -0300

    adding methods that[B were not showing

--- main.cpp
+++ main.cpp
@@ -2,41 +2,112 @@
 
 using namespace std;
 
-class ListaSequencial
+// A class that simulates a sequencial list with some methods.
+class SequentialList
 {
 private:
-    int *lista;
-    int tamanho;
-    int capacidade;
+    // Data members.
+
+    int *list;
+    int size;
+    int capacity;
+
 public:
-    ListaSequencial();
-    ListaSequencial(int capacidade);
-    ~ListaSequencial();
+    // Constructors and destructor.
+
+    SequentialList();
+    SequentialList(int capacity);
+    ~SequentialList();
+
+    // Methods.
+
+    int push_front(int value);
+    int push_back(int value);
+    int insert(int index, int value);
+    int pop_front();
+    int pop_back();
+    int remove_at(int index);
+    void size_of();
+    int find(int value);
+    int remove_all();
+    void sort();
+    void reverse();
 };
-/**
- * Default constructor that initializes variables with NULL and 0
-*/
-ListaSequencial::ListaSequencial()
-{
-    this->lista = NULL;
-    this->tamanho = 0;
-    this->capacidade = 0;
-}
-// 
-ListaSequencial::ListaSequencial(int capacidade)
-{
-    this->lista = new int[capacidade];
-    this->tamanho = tamanho;
-    this->capacidade = capacidade;
-}
-ListaSequencial::~ListaSequencial()
-{
-    delete[] this->lista;
-}
-
-
 
 int main()
 {
+    SequentialList list;
     return 0;
+}
+
+// Default constructor that initializes variables with NULL and 0.
+SequentialList::SequentialList()
+{
+    this->list = NULL;
+    this->size = 0;
+    this->capacity = 0;
+}
+// Parametrized constructor that allocates an array of int with the given capacity.
+SequentialList::SequentialList(int capacity)
+{
+    this->list = new int[capacity];
+    this->size = 0;
+    this->capacity = capacity;
+}
+// Destructor that destroys the array of int that the member list uses.
+SequentialList::~SequentialList()
+{
+    delete[] this->list;
+}
+
+int SequentialList::push_front(int value)
+{
+    return 1;
+}
+int SequentialList::push_back(int value)
+{
+    return 1;
+
+}
+int SequentialList::insert(int index, int value)
+{
+    return 1;
+    
+}
+int SequentialList::pop_front()
+{
+    return 1;
+    
+}
+int SequentialList::pop_back()
+{
+    return 1;
+    
+}
+int SequentialList::remove_at(int index)
+{
+    return 1;
+    
+}
+void SequentialList::size_of()
+{
+    
+}
+int SequentialList::find(int value)
+{
+    return 1;
+    
+}
+int SequentialList::remove_all()
+{
+    return 1;
+    
+}
+void SequentialList::sort()
+{
+    
+}
+void SequentialList::reverse()
+{
+    
 }
