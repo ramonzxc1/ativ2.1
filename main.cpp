d5a5b58f9cb8f6a88eb3d6ce378334e0484586b2
ramonzxc1 <ramonalternativeacc@gmail.com>
2023-05-17 00:30:28 -0300

    removed all the unnecessary this-

--- main.cpp
+++ main.cpp
@@ -43,120 +43,120 @@ int main()
 // Default constructor that initializes variables with NULL and 0.
 SequentialList::SequentialList()
 {
-    this->list = NULL;
-    this->size = 0;
-    this->capacity = 10;
+    list = NULL;
+    size = 0;
+    capacity = 10;
 }
 // Parametrized constructor that allocates an array of int with the given capacity.
 SequentialList::SequentialList(int capacity)
 {
-    this->list = new int[capacity];
-    this->size = 0;
-    this->capacity = capacity;
+    list = new int[capacity];
+    size = 0;
+    capacity = capacity;
 }
 // Destructor that destroys the array of int that the member list uses.
 SequentialList::~SequentialList()
 {
-    if(this->list != NULL) delete[] this->list;
+    if(list != NULL) delete[] list;
 }
 
 int SequentialList::push_front(int value)
 {
-    if(this->size < 0) return -1;
-    if(this->size+1 > this->capacity) return -1;
-    if(this->size == 0)
+    if(size < 0) return -1;
+    if(size+1 > capacity) return -1;
+    if(size == 0)
     {
-        this->size++;
-        this->list[0] = value;
+        size++;
+        list[0] = value;
         return 1;
     }
-    this->size++;
-    for(int i=this->size-1; i>0; i--)
+    size++;
+    for(int i=size-1; i>0; i--)
     {
-        this->list[i] = this->list[i-1];
+        list[i] = list[i-1];
     }
-    this->list[0] = value;
+    list[0] = value;
     return 1;
 }
 int SequentialList::push_back(int value)
 {
-    if(this->size < 0) return -1;
-    if(this->size+1 > this->capacity) return -1;
-    this->size++;
-    this->list[this->size-1] = value;
+    if(size < 0) return -1;
+    if(size+1 > capacity) return -1;
+    size++;
+    list[size-1] = value;
     return 1;
 }
 int SequentialList::insert(int index, int value)
 {
-    if(this->size < 0) return -1;
-    if(index > this->size) return -1;
+    if(size < 0) return -1;
+    if(index > size) return -1;
     if(index < 0) return -1;
 
     if(index == 0)
     {
-        this->push_front(value);
+        push_front(value);
         return 1;
     }
-    if(index == this->size)
+    if(index == size)
     {
-        this->push_back(value);
+        push_back(value);
         return 1;
     }    
     
-    this->size++;
-    for(int i=this->size-1; i>index; i--)
+    size++;
+    for(int i=size-1; i>index; i--)
     {
-        this->list[i] = this->list[i-1];
+        list[i] = list[i-1];
     }
-    this->list[index] = value;
+    list[index] = value;
     return 1;
 }
 int SequentialList::pop_front()
 {
-    if(this->size <= 0) return -1;
+    if(size <= 0) return -1;
 
-    for(int i=0; i<this->size-1; i++)
+    for(int i=0; i<size-1; i++)
     {
-        this->list[i] = this->list[i+1];
+        list[i] = list[i+1];
     }
-    this->size--;
+    size--;
     return 1;
     
 }
 int SequentialList::pop_back()
 {
-    if(this->size <= 0) return -1;
-    this->size--;
+    if(size <= 0) return -1;
+    size--;
     return 1;
     
 }
 int SequentialList::remove_at(int index)
 {
-    if(this->size <= 0) return -1;
+    if(size <= 0) return -1;
     if(index < 0) return -1;
-    if(index >= this->size) return -1;
+    if(index >= size) return -1;
 
-    if(index == this->size-1)
+    if(index == size-1)
     {
-        this->pop_back();
+        pop_back();
         return 1;
     }
     if(index == 0)
     {
-        this->pop_front();
+        pop_front();
         return 1;
     }
 
-    for(int i=index; i < this->size-1; i++)
+    for(int i=index; i < size-1; i++)
     {
-        this->list[i] = this->list[i+1];
+        list[i] = list[i+1];
     }
-    this->size--;
+    size--;
     return 1;
 }
 int SequentialList::size_of()
 {
-    return this->size;
+    return size;
 }
 int SequentialList::find(int value)
 {
@@ -164,14 +164,14 @@ int SequentialList::find(int value)
 
     // binary search
     int left = 0;
-    int right = this->size-1;
+    int right = size-1;
     while(left <= right)
     {
         int mid = left + (right-left)/2;
 
-        if(this->list[mid] == value) return 1;
+        if(list[mid] == value) return 1;
         
-        if(this->list[mid] < value) left = mid + 1;
+        if(list[mid] < value) left = mid + 1;
         else right = mid - 1;
     }
 
@@ -179,8 +179,8 @@ int SequentialList::find(int value)
 }
 int SequentialList::remove_all()
 {
-    if(this->size <= 0) return -1;
-    this->size = 0;
+    if(size <= 0) return -1;
+    size = 0;
     return 1;
 }
 void SequentialList::sort()
@@ -189,5 +189,11 @@ void SequentialList::sort()
 }
 void SequentialList::reverse()
 {
-    
+	int temp;
+    for(int i=0; i < size/2; i++)
+	{
+		temp = list[i];
+		list[i] = list[size-1-i];
+		list[size-1-i] = temp;
+	}
 }
